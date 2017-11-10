61aab985dad10b047a9a7e932d91fc6283382150
EGC1995 <eugene.echoe@gmail.com>
2017-11-10 00:08:58 -0600

    minor changes

--- AustinIncidentReport/Query.py
+++ AustinIncidentReport/Query.py
@@ -8,12 +8,26 @@ import pandas as pd
 import requests
 import numpy as np
 import json
+import matplotlib
+from bokeh.plotting import figure, output_file, show
 
 response = requests.get("https://data.austintexas.gov/resource/rkrg-9tez.json")
 data = response.json()
 
-# print(data)
 print(response.status_code)
-
 df = pd.DataFrame(data)
 print(df.columns)
+
+crimes = df['crime_type']
+latitude = df['latitude']
+longitude = df['longitude']
+time = df['time']
+date = df['date']
+
+output_file("plot.html")
+
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
+p = figure(title="crimes over time", x_axis_label = 'x', y_axis_label = 'y')
+p.line(x,y, legend = "Temp.", line_width = 2)
+show(p)
